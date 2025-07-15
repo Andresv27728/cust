21e5388adde8bc60a04861c7a9c253d483c9d960
Andresv27728 <andrescamilovallejogomez701@gmail.com>
2025-07-15 00:11:25 -0500

    Update hola.js

--- plugins/hola.js
+++ plugins/hola.js
@@ -2,6 +2,6 @@ export default {
     name: 'hola',
     description: 'Saluda al usuario',
     async call(m, { conn }) {
-        await conn.sendMessage(m.chat, { text: '¡Hola! ¿En qué puedo ayudarte?' }, { quoted: m })
+        await conn.sendMessage(m.chat, { text: '¡Hola! ¿En qué puedo ayudarte?' })
     }
 }
