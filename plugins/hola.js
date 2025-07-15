9b19ee246f397086d5011c5a94c9e58357d20f10
Andresv27728 <andrescamilovallejogomez701@gmail.com>
2025-07-15 00:56:25 -0500

    Delete plugins directory

--- plugins/hola.js
+++
@@ -1,7 +0,0 @@
-export default {
-  name: 'hola',
-  description: 'Saluda al usuario',
-  async call(m, { conn }) {
-    await conn.sendMessage(m.chat, { text: '¡Hola! ¿En qué puedo ayudarte?' }, { quoted: m }).catch(console.error)
-  }
-}
