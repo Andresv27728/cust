e268391c040001c6773e79041f09f2ccc8ca8d7f
Andresv27728 <andrescamilovallejogomez701@gmail.com>
2025-07-14 23:54:56 -0500

    Create hola.js

---
+++ plugins/hola.js
@@ -0,0 +1,7 @@
+export default {
+    name: 'hola',
+    description: 'Saluda al usuario',
+    async call(m, { conn }) {
+        await conn.sendMessage(m.chat, { text: '¡Hola! ¿En qué puedo ayudarte?' }, { quoted: m })
+    }
+}
